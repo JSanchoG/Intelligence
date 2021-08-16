1a412ebf3a2e921adea0d08b92931d786818c45e
ScorpionSquad <steamgozuk@gmail.com>
2021-08-16 21:52:02 +0200

    🔨 Preloader

--- assets/js/preloader.js
+++ assets/js/preloader.js
@@ -1,6 +1,4 @@
-var loader = document.getElementById("preloader-wrap");
-var login = document.getElementById("navbar-form");
-window.addEventListener("load", function(){
-    loader.style.display = "none";
-    login.style.display = "block";
+$(document).ready(function() {
+    $("#navbar-form").show();
+    $("#preloader-wrap").delay(2000).fadeOut("slow");
 });
