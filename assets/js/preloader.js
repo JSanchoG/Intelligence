7c27023bed0f0aac06e71a66167378773749a090
ScorpionSquad <steamgozuk@gmail.com>
2021-08-16 21:30:39 +0200

    ✨ Preloader

---
+++ assets/js/preloader.js
@@ -0,0 +1,6 @@
+var loader = document.getElementById("preloader-wrap");
+var login = document.getElementById("navbar-form");
+window.addEventListener("load", function(){
+    loader.style.display = "none";
+    login.style.display = "block";
+});
